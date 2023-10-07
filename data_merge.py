7f154e16af358a340f6091040874ea296db7b484
Lucas Holten <lhc.holten@gmail.com>
2023-10-07 15:14:43 +0200

    Update data_merge.py to only use subscriptions.json

--- data_merge.py
+++ data_merge.py
@@ -1,22 +1,27 @@
 import json
-  
+
 # Opening JSON file
-geo = open('locations.geojson')
-data = open('subscriptions.json')
-  
-data = json.load(data)
-geo = json.load(geo)
+with open('subscriptions.json') as file:
+    data = json.load(file)
 
-def get_area(name: str) -> str:
-    for thing in data["data"]:
-        if thing["name"] == name:
-            return thing["area"]
-    assert False, name
+geo = {
+    "type": "FeatureCollection",
+    "features": []
+}
 
-for f in geo["features"]:
-    name = f["properties"]["name"]
-    del f["properties"]["description"]
-    f["properties"]["area"] = get_area(name)
+for f in data["data"]:
+    geo["features"].append({
+        "type": "Feature", 
+        "geometry": {
+            "type": "Point", 
+            "coordinates": [f["long"], f["lat"]]
+        }, 
+        "properties": {
+            "name": f["name"], 
+            "area": f["area"],
+        },
+    })
 
-with open('locations_out.geojson', 'w', encoding='utf-8') as f:
-    json.dump(geo, f, ensure_ascii=False)
+
+with open('locations_out.geojson', 'w', encoding='utf-8') as file:
+    json.dump(geo, file, ensure_ascii=False)
